06770b51d757b667d82808a985b571924447577b
David Barnett <david@barnett.net.nz>
2016-05-02 11:39:16 +1200

    add tests for the relationships of City to routes

--- test/models/city_test.rb
+++ test/models/city_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class CityTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @city = cities(:one)
+    @to = Route.create(to: @city, provider: "AU")
+    @from = Route.create(from: @city, provider: "NZ")
+  end
+
+  def teardown
+    @to = nil
+    @from = nil
+    @city = nil
+  end
+
+  test "a city has routes to it" do
+    assert_equal @city.to, [@to]
+  end
+
+  test "a city has routes from it" do
+    assert_equal @city.from, [@from]
+  end
+
+  test "a city has routes" do
+    assert_equal @city.routes, [@to, @from]
+  end
+
 end
