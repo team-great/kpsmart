a51329f22fbb0bae82649841bc5fcb9c69a341d7
Cameron <cameron-prc@github.com>
2016-05-12 12:41:03 +1200

    added todos for the remainder of the skeleton

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,15 +1,18 @@
 class ReportsController < ApplicationController
 
 
+  # Requires route finding logic
   def path_summary
 
-
+    #TODO
+    #for each mail_delivery, calculate all values associated with it and display it the the user
 
   end
 
   def revenue
 
-
+    #TODO
+    #for each mail delivery, sum the value that is being brought in
 
   end
 
--- app/models/mail_delivery.rb
+++ app/models/mail_delivery.rb
@@ -1,3 +1,5 @@
 class MailDelivery < ActiveRecord::Base
   has_and_belongs_to_many :routes
+
+  #represents a single peice of mail traveling through the system
 end
