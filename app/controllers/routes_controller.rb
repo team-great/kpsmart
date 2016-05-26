e1be4ebc5af2ed2e40b27cbba65720ad473486f1
cuan lategan <lategacuan@myvuw.ac.nz>
2016-05-26 16:59:48 +1200

    search seems to work, also handles case where path is not found

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -4,25 +4,6 @@ class RoutesController < ApplicationController
   # GET /routes.json
   def index
 
-
-    r = [[1, 2, 1],
-         [1, 3, 9],
-         [1, 5, 3],
-         [2, 4, 3],
-         [2, 3, 7],
-         [4, 3, 2],
-         [4, 1, 1],
-         [5, 2, 4],
-         [5, 4, 2]]
-
-    start_point = 1 # starting node
-    end_point = 3 # arrival node
-
-    ob = Dijkstra.new(start_point, end_point, r)
-
-    #puts "Cost = #{ob.cost}"
-    #puts "Shortest Path from #{start_point} to #{end_point} = #{ob.shortest_path}"
-    @path_results = ob
     @routes = Route.all
 
   end
