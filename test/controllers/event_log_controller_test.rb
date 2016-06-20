c78d0332d67dd852f4981d7981d40c0393ad652c
David Barnett <david@barnett.net.nz>
2016-06-20 13:22:35 +1200

    fix event log not rendering properly and tests

--- test/controllers/event_log_controller_test.rb
+++ test/controllers/event_log_controller_test.rb
@@ -6,4 +6,32 @@ class EventLogControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get index with mail delivery" do
+    create(:mail, :with_route)
+    get :index
+    assert_response :success
+    assert_template :partial => '_mail_event'
+  end
+
+  test "should get index with route creation" do
+    with_versioning do
+      create(:route)
+      get :index
+      assert_response :success
+      assert_template :partial => '_create_event'
+    end
+  end
+
+  test "should get index with route edit" do
+    with_versioning do
+      route = create(:route)
+      route.max_weight = 101
+      route.save
+
+      get :index
+      assert_response :success
+      assert_template :partial => '_update_event'
+    end
+  end
+
 end
